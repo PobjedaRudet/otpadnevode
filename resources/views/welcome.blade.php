37f36a3c44c86ac549be5cf651422be6f433b799
Ahmet Hadžiahmetović <info@pobjeda.com>
2025-10-01 13:42:01 +0200

    dodana slika na naslovnu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
 
 
-                    <img src="{{ asset('/images/precistac-light.jpg') }}" alt="Prečistač" class="w-full h-auto object-cover" />
+                    <img src="{{ asset('/images/otpadne_vode.jpg') }}" alt="Prečistač" class="w-full h-auto object-cover" />
                     <div class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
                 </div>
             </main>
